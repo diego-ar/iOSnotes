8409f9a84ee04c6eb445bb11026f677071ac570e
Diego <122388907+diego-ar@users.noreply.github.com>
2024-08-07 22:52:41 +0200

    Update Notes#1.swift

--- Notes#1.swift
+++ Notes#1.swift
@@ -40,3 +40,94 @@ print(myBool)
 let myConst = "Mi propiedad constante"
 //myConst = "Mi nueva propiedad constante"// error
 
+//Opcionales
+
+var myOptional: String? = "Mi Opcional"
+myOptional = nil
+
+//comprobar nil !!Peligroso
+if myOptional != nil {
+    
+print(myOptional)
+}
+
+//Control de Flujo
+
+if myInt == 10 && myString == "Hola"{
+   print("El valor es 10")
+}else if myInt == 11 || myString == "Hola"{
+    print("El valor es 11")
+}
+else{
+    print "El valor no es 10 ni 11"
+}
+
+//Listas (Ordenadas)
+
+var myList = ["Diego","diego-ar98", "Diego Alfageme Rodriguez"]
+print(myList)
+print(myList[0])
+print(myList[1])
+myList.append("35")
+print(myList)
+
+//Sets (Desordenado)
+var mySets: Sets = ["Ruby", "Swift", "HTML5","Java"]
+
+//Mapas
+var myMap = ["Diego":26]
+print(myMap)
+
+//desempaquetado expliito de nulos
+
+if let age = myMap["Kontrol"] {
+    print(age)
+}else{
+    print("No existe esta clave")
+}
+print (myMap["Kontrol"] ?? "No existe esta clave")
+
+//Bucles
+
+for value in myList {
+    print(value)
+}
+
+var index = 0
+whila index < myList.count {
+    print(myList[index])
+    index += 1
+    
+}
+
+//Funciones
+
+func myFunc() -> String {
+    return "Esto es una Funcion"
+}
+
+//Clases
+
+class MyClass {
+    var name: String?
+    var age: Int?
+}
+
+var myClass = MyClass()
+myClass.name = "Diego"
+myClass.age = 26
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
