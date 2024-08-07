3fbd4a66a39ceb8e4985ddfbfc2c69588b46ea9b
Diego <122388907+diego-ar@users.noreply.github.com>
2024-08-07 20:55:27 +0200

    Update Notes#1.swift

--- Notes#1.swift
+++ Notes#1.swift
@@ -3,5 +3,40 @@ import Foundation
 //hello world
 print ("Hola Swift!")
 
-//
+//Esto es una linea de comentario 
+
+/* 
+Esto es un bloque de comentario
+*/
+
+//Tipado
+con :string
+    :double
+    :
+
+//Variable
+var aString = "Esto es una cadena de texto"
+print(aString)
+
+var aString2: String = "Esto es otra cadena de texto"
+print(aString2)
+
+var myInt = 6
+var myInt2: Int = 5
+print(myInt2)
+print(myInt - myInt2)
+
+var myDouble = 5.5
+print(myDouble)
+
+var myFloat: Float = 4.8
+print(myFloat)
+
+var myBool = true
+print(myBool)
+
+//Constantes
+
+let myConst = "Mi propiedad constante"
+//myConst = "Mi nueva propiedad constante"// error
 
